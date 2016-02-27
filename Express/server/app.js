15ef5d87281c6ea2aeb0883022b91e02b26be2b4
Sharon Rumsey <sharon@rumsey.nz>
2016-02-27 20:18:18 +1300

    app running on server 3000

--- Express/server/app.js
+++ Express/server/app.js
@@ -1,10 +1,15 @@
 var express = require('express');
+var path = require('path')
 
 var app = express();
 
 app.get('/', function(req, res) {
 	res.send('Hello World');
-})
+});
+
+app.get('/about', function(req, res) {
+	res.sendFile(path.join(__dirname,'about.html'));
+});
 
 app.listen(3000, function() {
 	console.log('server started on port 3000')
