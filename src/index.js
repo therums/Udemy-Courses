adf82c7f14586b5e8cd07ab25e305b200d77ce75
Sharon Rumsey <sharon@rumsey.nz>
2016-03-13 12:31:42 +1300

    search bar working

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
-// import VideoListItem from './components/video_list_item';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyDP1iobS0jf9j-cdl826gQMfuXEIc5nlmY';
 
@@ -11,17 +11,31 @@ class App extends Component {
 	constructor(props) {
 		super(props);
 
-		this.state = { videos: [] };
+		this.state = { 
+			videos: [],
+			selectedVideo: null 
+		};
 
-		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-				this.setState({ videos });
+		this.videoSearch('surfboards');
+	}
+
+	videoSearch(term) {
+		YTSearch({key: API_KEY, term: term}, (videos) => {
+				this.setState({ 
+					videos: videos,
+					selectedVideo: videos[0]
+				});
 		});
 	}
+
 	render() {
 		return (
 			<div>
-				<SearchBar />
-				<VideoList videos={this.state.videos} />
+				<SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+				<VideoDetail video={this.state.selectedVideo} />
+				<VideoList 
+					onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+					videos={this.state.videos} />
 			</div>
 		);
 	}
