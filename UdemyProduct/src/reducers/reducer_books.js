c7fc7cec678609589b3c6679b1c838c1502348f0
Sharon Rumsey <sharon@rumsey.nz>
2016-03-13 16:01:22 +1300

    setting up books

---
+++ UdemyProduct/src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+// function that will return an array of books
+
+export default function() {
+	return [
+		{title: 'Javascript'},
+		{title: 'Harry Potter'},
+		{title: 'The Three Towers'}
+		{title: 'Eloquent Javascript'}
+	]
+}
